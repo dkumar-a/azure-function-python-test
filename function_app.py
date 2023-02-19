6468850ae298ffee14dbe9690ac15819e63c8688
Dinesh Kumar Anbazhagan <dkumara@zohomail.com>
2023-02-18 19:17:03 -0600

    Added Opencensus

--- function_app.py
+++ function_app.py
@@ -13,7 +13,7 @@ app = func.FunctionApp()
 
 @app.function_name(name="HttpTrigger1")
 @app.route(route="hello")
-def main(req: func.HttpRequest) -> func.HttpResponse:
+def test_function(req: func.HttpRequest) -> func.HttpResponse:
     logger.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
@@ -31,27 +31,4 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     else:
         return func.HttpResponse(
             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.", status_code=200
-        )
-
-
-@app.function_name(name="HttpTrigger1")
-@app.route(route="hello")
-def test_function(req: func.HttpRequest) -> func.HttpResponse:
-    logger.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-            status_code=200
         )
