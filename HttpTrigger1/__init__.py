33ff9a28fae25aa6ef09d7a92f57ddd6d9f68105
Dinesh Kumar Anbazhagan <dkumara@zohomail.com>
2023-02-18 20:07:36 -0600

    Replace with Python V1

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -1,19 +1,9 @@
 import azure.functions as func
 import shared_logging
 
-logger = shared_logging.getLogger(__name__)
 
-
-app = func.FunctionApp()
-
-# Learn more at aka.ms/pythonprogrammingmodel
-
-# Get started by running the following code to create a function using a HTTP trigger.
-
-
-@app.function_name(name="HttpTrigger1")
-@app.route(route="hello")
-def test_function(req: func.HttpRequest) -> func.HttpResponse:
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logger = shared_logging.getLogger(__name__)
     logger.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
@@ -30,5 +20,6 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.", status_code=200
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200
         )
